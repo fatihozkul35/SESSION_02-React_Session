df5f9fa98a772c58b0291888a6ff5bd8868eb1fb
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-07 13:52:26 +0100

    first commit

---
+++ rc01_install_starter/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO REACT</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ rc02_jsx/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>HELLO REACT</h1>
+    </div>
+  );
+}
+
+export default App;
